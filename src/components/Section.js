744fa56281fd16085f3ee94e38c17de530efe3dc
sabeena1217 <sabeena1217@gmail.com>
2021-10-24 11:43:14 +0530

    ADD: redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     // console.log(props)
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
-                    {rightBtnText && 
-                        <RightButton>
-                            {rightBtnText}
-                        </RightButton>
-                    }
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {rightBtnText &&
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg">
                 </DownArrow>
             </Buttons>
@@ -40,7 +45,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between; // vertical
     align-items: center;  // horizontal
-    background-image: ${props => `url("/images/${props.bgImage}")` };
+    background-image: ${props => `url("/images/${props.bgImage}")`};
 `
 
 const ItemText = styled.div`
